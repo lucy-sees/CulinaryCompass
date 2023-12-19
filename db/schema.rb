6df38a78753f1ea665f5737ee0a194b8e683bfdb
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-12-19 16:40:30 +0300

    Add email confirmation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_19_120216) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_19_132359) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,8 +56,14 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_19_120216) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unconfirmed_email"], name: "index_users_on_unconfirmed_email", unique: true
   end
 
   add_foreign_key "foods", "users"
