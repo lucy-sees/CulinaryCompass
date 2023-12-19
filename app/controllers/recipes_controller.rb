6a078891bc768602044b0b10b3fa26da5edfa8e5
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-12-19 13:24:28 +0300

    fix additional linter errors

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,8 +7,7 @@ class RecipesController < ApplicationController
   end
 
   # GET /recipes/1 or /recipes/1.json
-  def show
-  end
+  def show; end
 
   # GET /recipes/new
   def new
@@ -16,8 +15,7 @@ class RecipesController < ApplicationController
   end
 
   # GET /recipes/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /recipes or /recipes.json
   def create
