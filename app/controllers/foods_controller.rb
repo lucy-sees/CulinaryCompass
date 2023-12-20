ad0c7a75d25079e40c8c11b3b4d04c8f66bf1dda
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-12-20 15:16:07 +0300

    Create Food list

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,9 +1,10 @@
 class FoodsController < ApplicationController
   before_action :set_food, only: %i[show edit update destroy]
+  before_action :check_owner, only: %i[edit update destroy]
 
   # GET /foods or /foods.json
   def index
-    @foods = Food.all
+    @foods = current_user.foods
   end
 
   # GET /foods/1 or /foods/1.json
@@ -19,11 +20,11 @@ class FoodsController < ApplicationController
 
   # POST /foods or /foods.json
   def create
-    @food = Food.new(food_params)
+    @food = current_user.foods.build(food_params)
 
     respond_to do |format|
       if @food.save
-        format.html { redirect_to food_url(@food), notice: 'Food was successfully created.' }
+        format.html { redirect_to foods_path, notice: 'Food was successfully created!' }
         format.json { render :show, status: :created, location: @food }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -47,10 +48,10 @@ class FoodsController < ApplicationController
 
   # DELETE /foods/1 or /foods/1.json
   def destroy
-    @food.destroy!
+    @food.destroy
 
     respond_to do |format|
-      format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
+      format.html { redirect_to foods_path, notice: 'Food was successfully deleted!' }
       format.json { head :no_content }
     end
   end
@@ -64,6 +65,12 @@ class FoodsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def food_params
-    params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
+
+  def check_owner
+    unless @food.user == current_user
+      redirect_to foods_path, alert: 'You can only edit or delete your own foods.'
+    end
   end
 end
