676409805441c89fdba35d7167bed2c5253e4d84
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-12-20 17:22:13 +0300

    Make corrections to ensure everything works as expected

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
+
+  validates :name, uniqueness: { scope: :user_id, message: 'You have already created a recipe with this name!' }
 end
