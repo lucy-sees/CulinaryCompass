b529129cc4c06bd0e85ca3ea6090ff21567ba5b4
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-12-22 17:03:04 +0300

    minor corrections

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -97,6 +97,6 @@ class RecipesController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def recipe_params
-    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id)
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id, :steps)
   end
 end
